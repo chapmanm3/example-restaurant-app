b55908b55b5e6dade5e47fb5317d80c3b7478c95
Matt Chapman <chapmancac@gmail.com>
2024-06-11 18:30:20 -0400

    home page work

--- src/components/HomePageBody/HomePageBody.tsx
+++ src/components/HomePageBody/HomePageBody.tsx
@@ -4,7 +4,7 @@ import styles from "./homePageBody.module.css"
 export default function HomePageBody() {
   return (
     <div className={styles.homePageContainer}>
-      <h2>Matt's Taco Shop</h2>
+      <h2 className={styles.nameText}>Matt's Taco Shop</h2>
       <div className={styles.locationDateContainer}>
         <h4>1234 Not Real Dr.<br/>Haslet TX 76052</h4>
         <p>
